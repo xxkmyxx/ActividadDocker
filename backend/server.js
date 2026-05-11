af077319f98ddad06d414a00e6ebf07d7134de2b
MA-Alumno <MA-Alumno@duoc.acad>
2026-05-11 19:44:16 -0400

    Cambiando IPs del back, db y front

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const {
-  DB_HOST = "10.0.2.134", // acá colocar la IP Privada EC2 DB
+  DB_HOST = "10.0.146.254", // acá colocar la IP Privada EC2 DB
   DB_USER = "root",
   DB_PASSWORD = "admin123",
   DB_NAME = "tienda_perritos",
